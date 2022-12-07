0f979d44e5f48b3efb6a22332dc53ac30835ec36
MariaLBovin <maria.bovin@gmail.com>
2022-12-07 09:19:49 +0100

    new push-try

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,49 @@
 import './style/style.scss';
 
-// All kod härifrån och ner är bara ett exempel för att komma igång
+// import killersArray from './utils/killers';
 
-// I denna utils-fil har vi lagrat funktioner som ofta används, t.ex. en "blanda array"-funktion
-import { shuffle } from './utils';
+const killersArray = [
+  {
+    id: 1,
+    name: 'killer1',
+    age: 25,
+    haircolor: 'red',
+    image: 'images/test.jpg',
+  },
+  {
+    id: 2,
+    name: 'killer2',
+    age: 41,
+    haircolor: 'brown',
+    image: 'images/test.jpg',
+
+  },
+  {
+    id: 3,
+    name: 'killer3',
+    age: 39,
+    haircolor: 'blonde',
+    image: 'images/test.jpg',
+  },
+  {
+    id: 4,
+    name: 'killer4',
+    age: 65,
+    haircolor: 'black',
+    image: 'images/test.jpg',
+  },
+  {
+    id: 5,
+    name: 'killer5',
+    age: 32,
+    haircolor: 'red',
+    image: 'images/test.jpg',
+  },
+  {
+    id: 6,
+    name: 'killer6',
+    age: 11,
+    haircolor: 'blonde',
+    image: 'images/test.jpg',
+  },
+];
---
+++ src/style/utils/killers.js
@@ -0,0 +1,45 @@
+const killersArray=[
+    {
+      id:1,
+      name:'killer1',
+      age: 25,
+      haircolor:'red',
+      image: ,
+    },
+    {
+      id:2,
+      name:'killer2',
+      age: 41,
+      haircolor:'brown',
+      image: ,
+  
+    },
+    {
+      id:3,
+      name:'killer3',
+      age: 39,
+      haircolor:'blonde',
+      image: ,
+    },
+    {
+      id:4,
+      name:'killer4',
+      age: 65,
+      haircolor:'black',
+      image: ,
+    },
+    {
+      id:5,
+      name:'killer5',
+      age: 32,
+      haircolor:'red',
+      image: ,
+    },
+    {
+      id:6,
+      name:'killer6',
+      age: 11,
+      haircolor:'blonde',
+      image: ,
+    },
+  ]

321e49137b249308eda4a9b72e04538deea525b7
MariaLBovin <maria.bovin@gmail.com>
2022-12-08 16:32:10 +0100

    select-killer button added

--- src/main.js
+++ src/main.js
@@ -1,26 +1,32 @@
 import './style.scss';
 //import {killersArray} from '.arrays.js';
 
-const beginGameBtn = document.querySelector('.begin-game');
-console.log(beginGameBtn)
-beginGameBtn.addEventListener('click', startGame);
+/************************************************************************    
+*                           Globala variabler 
+*************************************************************************/
+const startPage = document.querySelector('.startpage');
+const selectionPage = document.querySelector('#selectionpage');
 
-function startGame(){
-/**
- * Vad ska hända: Man ska klicka på knappen och då ska spelsidan med alla kort
- * öppnas.
- * Längst upp ska det finnas en text där det står vad man ska göra, 
- * längst ner ska det finnas en knapp där man kan testa sitt val.
- * 
-*/
-}
-startGame ()
+/***********************************************************************
+ *                         Funktioner
+ ***********************************************************************/
+
+//Starta spelet. När man klickar på knappen döljs startsidan spelsidan öppnas.
+const beginGameBtn = document.querySelector('.begin-game');
+beginGameBtn.addEventListener('click', () => {
+  startPage.style.display = 'none';
+  selectionPage.classList.remove('toggle-hidden');
+});
+
+// Alla mördare. Informationen används av display-killers-funktionen
 const killersArray = [
   {
     id: 1,
     name: 'Fru Guldhov',
     age: 65,
     color: 'Röd',
+    about:
+      'Fru Guldhov, ägare till stall Guldhoven. <br> 65 år gammal, går alltid klädd i blå ridkläder och ridstövlar.<br> Sträng men rättvis.',
     image: 'public/images/killers/fru_guldhov.jpg',
   },
   {
@@ -60,59 +66,93 @@ const killersArray = [
   },
 ];
 
+//Skriver ut alla mördare till spelsidan
 function displayKillers() {
   const killersPlacement = document.querySelectorAll('.killer');
 
   for (let i = 0; i < killersArray.length; i++) {
     let killerNr = 'killer-' + i;
+    let killerTxt = 'killertxt' + i;
     const printKillers = `<div class="${killerNr}">
-                <img id="img-${i}" src="${killersArray[i].image}" alt="" width="50" height="100">
-            <h2>${killersArray[i].name}</h2>`;
+        <img id="img-${i}" src="${killersArray[i].image}" alt="" width="50" height="100">
+        <h2>${killersArray[i].name}</h2>
+        <button id="killerbutton-${i}" class="select-killer">Välj</button>
+        </div>`;
     killersPlacement[i].innerHTML = printKillers;
   }
 }
+
 displayKillers();
 
+/*
+Funktion för att kunna klicka på ett spelkort och läsa mer om personen
+const killerTextLong = document.querySelectorAll('.read-more');
+
+const readMore = document.querySelectorAll('img');
+console.log(readMore)
+readMore.addEventListener('click', () => {
+  killerTextLong.classList.remove('toggle-hidden');
+});
+*/
+
+//Alla platser. Används av funktionen displayPlaces
+
+const pickKillerBtn = document.querySelectorAll('.select-killer');
+  pickKillerBtn.forEach ((btn) => {
+    btn.addEventListener('click', pickKiller)
+    
+  })
+
+function pickKiller (btn){
+  let currentBtn = btn.currentTarget.id
+  console.log(currentBtn)
+  currentBtn.classList.add('.active')
+  
+}
+
 const placesArray = [
-    {
+  {
     place: 'I boxen',
     image: 'public/images/places/hastbox.jpg',
-    },
-    {
+  },
+  {
     place: 'I hagen',
     image: 'public/images/places/hasthage.jpg',
-    },
-    {
+  },
+  {
     place: 'I paddocken',
     image: 'public/images/places/paddock.jpg',
-    },
-    {
+  },
+  {
     place: 'I höladan',
     image: 'public/images/places/holada.jpg',
-    },
-    {
+  },
+  {
     place: 'I ridhuset',
     image: 'public/images/places/ridhus.jpg',
-    },
-    {
+  },
+  {
     place: 'I hästtransporten',
     image: 'public/images/places/transport.jpg',
-    },
-]
-function displayPlaces() {
-    const placesPlacement = document.querySelectorAll('.place');
-      console.log(placesPlacement)
-    for (let i = 0; i < placesArray.length; i++) {
-      console.log(placesArray)
-      let placeNr = 'place-' + i;
-      const printPlaces = `<div class="${placeNr}">
-                    <img id="img-${i}" src="${placesArray[i].image}" alt="" width="50" height="100">
-                <h2>${placesArray[i].place}</h2>`;
-      placesPlacement[i].innerHTML = printPlaces;
-    }
-  }
-  displayPlaces();
+  },
+];
 
+//Skriver ut alla platser till spelsidan
+function displayPlaces() {
+  const placesPlacement = document.querySelectorAll('.place');
+  for (let i = 0; i < placesArray.length; i++) {
+    let placeNr = 'place-' + i;
+    const printPlaces = `<div class="${placeNr}">
+        <img id="img-${i}" src="${placesArray[i].image}" alt="" width="50" height="100">
+        <h2>${placesArray[i].place}</h2>
+        
+      </div>`;
+    placesPlacement[i].innerHTML = printPlaces;
+  }
+}
+displayPlaces();
+
+//Alla vapen. Används av funktionen displayWeapons
 const weaponsArray = [
   {
     id: 1,
@@ -145,11 +185,11 @@ const weaponsArray = [
     image: 'public/images/weapons/ridspo.jpg',
   },
 ];
+
+//Skriver ut alla vapen till spelsidan.
 function displayWeapons() {
   const weaponsPlacement = document.querySelectorAll('.weapon');
-    console.log(weaponsPlacement)
   for (let i = 0; i < weaponsArray.length; i++) {
-    console.log(weaponsArray)
     let weaponNr = 'weapon-' + i;
     const printWeapons = `<div class="${weaponNr}">
                   <img id="img-${i}" src="${weaponsArray[i].image}" alt="" width="50" height="100">

b908fd65adb92cdd8821c6df93c2c03b1649adf1
MariaLBovin <maria.bovin@gmail.com>
2022-12-09 13:48:56 +0100

    buttons-functionality added

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import './style.scss';
 //import {killersArray} from '.arrays.js';
 
-/************************************************************************    
-*                           Globala variabler 
-*************************************************************************/
+/************************************************************************
+ *                           Globala variabler
+ *************************************************************************/
 const startPage = document.querySelector('.startpage');
 const selectionPage = document.querySelector('#selectionpage');
 
@@ -27,42 +27,42 @@ const killersArray = [
     color: 'Röd',
     about:
       'Fru Guldhov, ägare till stall Guldhoven. <br> 65 år gammal, går alltid klädd i blå ridkläder och ridstövlar.<br> Sträng men rättvis.',
-    image: 'public/images/killers/fru_guldhov.jpg',
+    image: '/images/killers/fru_guldhov.jpg',
   },
   {
     id: 2,
     name: 'Herr Guldhov',
     age: 69,
     color: 'Gul',
-    image: 'public/images/killers/herr_guldhov.jpg',
+    image: '/images/killers/herr_guldhov.jpg',
   },
   {
     id: 3,
     name: 'Kalle Knackare',
     age: 39,
     color: 'Grön',
-    image: 'public/images/killers/kalle_knackare.jpg',
+    image: '/images/killers/kalle_knackare.jpg',
   },
   {
     id: 4,
     name: 'Anna La Rouge',
     age: 23,
     color: 'Röd',
-    image: 'public/images/killers/anna_la_rouge.jpg',
+    image: '/images/killers/anna_la_rouge.jpg',
   },
   {
     id: 5,
     name: 'Rosa Rosando',
     age: 18,
     color: 'Rosa',
-    image: 'public/images/killers/rosa_rosando.jpg',
+    image: '/images/killers/rosa_rosando.jpg',
   },
   {
     id: 6,
     name: 'Pjotr Plum',
     age: 42,
     color: 'Plommon',
-    image: 'public/images/killers/pjotr_plum.jpg',
+    image: '/images/killers/pjotr_plum.jpg',
   },
 ];
 
@@ -76,7 +76,7 @@ function displayKillers() {
     const printKillers = `<div class="${killerNr}">
         <img id="img-${i}" src="${killersArray[i].image}" alt="" width="50" height="100">
         <h2>${killersArray[i].name}</h2>
-        <button id="killerbutton-${i}" class="select-killer">Välj</button>
+        <button id="killerbutton-${i}" class="select-killer selectbutton">Välj</button>
         </div>`;
     killersPlacement[i].innerHTML = printKillers;
   }
@@ -85,47 +85,47 @@ function displayKillers() {
 displayKillers();
 
 //Funktion för att välja mördare
-
 const pickKillerBtn = document.querySelectorAll('.select-killer');
-  pickKillerBtn.forEach ((btn) => {
-    btn.addEventListener('click', pickKiller)
-    
-  })
+pickKillerBtn.forEach(btn => {
+  btn.addEventListener('click', pickKiller);
+});
 
-function pickKiller (btn){
-  let currentBtn = btn.currentTarget.id
-  console.log(currentBtn)
-  currentBtn.classList.add('.active')
-  
+function pickKiller(btn) {
+  let selectedKilllerBtn = btn.currentTarget;
+  selectedKilllerBtn.classList.add('active');
+
+  pickKillerBtn.forEach(button => {
+    if (!button.classList.contains('active')) {
+      button.setAttribute('disabled', 'disabled');
+    }
+  });
 }
 
 //Alla platser. Används av funktionen displayPlaces
-
-
 const placesArray = [
   {
     place: 'I boxen',
-    image: 'public/images/places/hastbox.jpg',
+    image: '/images/places/hastbox.jpg',
   },
   {
     place: 'I hagen',
-    image: 'public/images/places/hasthage.jpg',
+    image: '/images/places/hasthage.jpg',
   },
   {
     place: 'I paddocken',
-    image: 'public/images/places/paddock.jpg',
+    image: '/images/places/paddock.jpg',
   },
   {
     place: 'I höladan',
-    image: 'public/images/places/holada.jpg',
+    image: '/images/places/holada.jpg',
   },
   {
     place: 'I ridhuset',
-    image: 'public/images/places/ridhus.jpg',
+    image: '/images/places/ridhus.jpg',
   },
   {
     place: 'I hästtransporten',
-    image: 'public/images/places/transport.jpg',
+    image: '/images/places/transport.jpg',
   },
 ];
 
@@ -137,44 +137,62 @@ function displayPlaces() {
     const printPlaces = `<div class="${placeNr}">
         <img id="img-${i}" src="${placesArray[i].image}" alt="" width="50" height="100">
         <h2>${placesArray[i].place}</h2>
-        
+        <button id="placesbutton-${i}" class="select-place selectbutton ">Välj</button>
       </div>`;
     placesPlacement[i].innerHTML = printPlaces;
   }
 }
 displayPlaces();
 
+//Funktion för att välja plats
+const pickPlacesBtn = document.querySelectorAll('.select-place');
+pickPlacesBtn.forEach(btn => {
+  btn.addEventListener('click', pickPlace);
+});
+
+function pickPlace(btn) {
+  let selectedPlace = btn.currentTarget;
+  console.log(selectedPlace);
+  selectedPlace.classList.add('active');
+
+  pickPlacesBtn.forEach(button => {
+    if (!button.classList.contains('active')) {
+      button.setAttribute('disabled', 'disabled');
+    }
+  });
+}
+
 //Alla vapen. Används av funktionen displayWeapons
 const weaponsArray = [
   {
     id: 1,
-    weapon: 'Ett grimskaft',
-    image: 'public/images/weapons/grimskaft.jpg',
+    weapon: 'Grimskaft',
+    image: '/images/weapons/grimskaft.jpg',
   },
   {
     id: 2,
-    weapon: 'En hovkrats',
-    image: 'public/images/weapons/hovkrats.jpg',
+    weapon: 'Hovkrats',
+    image: '/images/weapons/hovkrats.jpg',
   },
   {
     id: 3,
-    weapon: 'En hoppbom',
-    image: 'public/images/weapons/hoppbom.jpg',
+    weapon: 'Hoppbom',
+    image: '/images/weapons/hoppbom.jpg',
   },
   {
     id: 4,
-    weapon: 'En bedövningsspruta',
-    image: 'public/images/weapons/bedovningspistol.jpg',
+    weapon: 'Bedövningsspruta',
+    image: '/images/weapons/bedovningspistol.jpg',
   },
   {
     id: 5,
-    weapon: 'En hammare',
-    image: 'public/images/weapons/hammare.jpg',
+    weapon: 'Hammare',
+    image: '/images/weapons/hammare.jpg',
   },
   {
     id: 6,
-    weapon: 'Ett ridspö',
-    image: 'public/images/weapons/ridspo.jpg',
+    weapon: 'Ridspö',
+    image: '/images/weapons/ridspo.jpg',
   },
 ];
 
@@ -185,11 +203,33 @@ function displayWeapons() {
     let weaponNr = 'weapon-' + i;
     const printWeapons = `<div class="${weaponNr}">
                   <img id="img-${i}" src="${weaponsArray[i].image}" alt="" width="50" height="100">
-              <h2>${weaponsArray[i].weapon}</h2>`;
+              <h2>${weaponsArray[i].weapon}</h2>
+              <button id="weaponbutton-${i}" class="select-weapon selectbutton">Välj</button>
+              </div>`
+              ;
     weaponsPlacement[i].innerHTML = printWeapons;
   }
 }
 displayWeapons();
+
+//Funktion för att välja plats
+
+const pickWeaponsBtn = document.querySelectorAll('.select-weapon');
+pickWeaponsBtn.forEach(btn => {
+  btn.addEventListener('click', pickWeapon);
+});
+
+function pickWeapon(btn) {
+  let selectedWeapon = btn.currentTarget;
+  console.log(selectedWeapon);
+  selectedWeapon.classList.add('active');
+
+  pickWeaponsBtn.forEach(button => {
+    if (!button.classList.contains('active')) {
+      button.setAttribute('disabled', 'disabled');
+    }
+  });
+}
 
 /*
 Funktion för att kunna klicka på ett spelkort och läsa mer om personen
